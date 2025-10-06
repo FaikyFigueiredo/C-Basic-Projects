bb1416ee35375e533b7f849f026e205fd2beaceb
FaikyFigueiredo <contato.fabiohenriquefigueiredo@gmail.com>
2025-10-06 14:03:27 -0300

    A program that verify if a CPF is valid or not

--- verifyCPF.c
+++ verifyCPF.c
@@ -17,23 +17,28 @@ int sumBiggers(char nums1[3], char nums2[3], char nums3[3]){
 int main(){
     char CPF[14] , firstNums[3] , secondNums[3], thirdNums[3], result;
     int expectedResult;
-    printf("Put your CPF (brazzilian code to know who is the person) and I will verify if it's a valid CPF");
-    scanf("%s" , CPF);
-    firstNums[0] = CPF[0];
-    firstNums[1] = CPF[1];
-    firstNums[2] = CPF[2];
-    secondNums[0] = CPF[4];
-    secondNums[1] = CPF[5];
-    secondNums[2] = CPF[6];
-    thirdNums[0] = CPF[8];
-    thirdNums[1] = CPF[9];
-    thirdNums[2] = CPF[10];
-    result = CPF[12];
-    expectedResult = sumBiggers(firstNums , secondNums , thirdNums)%10;
-    if (result - '0' == expectedResult){
-        printf("The digit %c is valid for this CPF" , result);
-    }else{
-        printf("The digit %c is invalid for this CPF, the valid digit is %d" , result, expectedResult);
+    while(1){
+        printf("Put your CPF (brazzilian code to know who is the person) and I will verify if it's a valid CPF");
+        scanf("%s" , CPF);
+        if(CPF[0] == 'F'){
+            break;
+        }
+        firstNums[0] = CPF[0];
+        firstNums[1] = CPF[1];
+        firstNums[2] = CPF[2];
+        secondNums[0] = CPF[4];
+        secondNums[1] = CPF[5];
+        secondNums[2] = CPF[6];
+        thirdNums[0] = CPF[8];
+        thirdNums[1] = CPF[9];
+        thirdNums[2] = CPF[10];
+        result = CPF[12];
+        expectedResult = sumBiggers(firstNums , secondNums , thirdNums)%10;
+        if (result - '0' == expectedResult){
+            printf("The digit %c is valid for this CPF" , result);
+        }else{
+            printf("The digit %c is invalid for this CPF, the valid digit is %d" , result, expectedResult);
+        }
     }
     return 0;
 }
