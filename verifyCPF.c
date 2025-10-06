83ea8731df74a0fa5e6bd1228f32d69765f227e2
FaikyFigueiredo <contato.fabiohenriquefigueiredo@gmail.com>
2025-10-06 14:01:02 -0300

    A program that verify if a CPF is valid or not

--- verifyCPF.c
+++ verifyCPF.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-int sumBiggers(nums1[3], nums2[3], nums3[3]){
-
+int getBigger(char nums[3]){
+    int result = 0;
+    for (int i=0 ; i<3 ; i++){
+        if (nums[i] - '0' > result){
+            result = nums[i] - '0';
+        }
+    }
+    return result;
+}
+int sumBiggers(char nums1[3], char nums2[3], char nums3[3]){
+    int result = 0; 
+    result += getBigger(nums1) + getBigger(nums2) + getBigger(nums3);
+    return result;
 }
 int main(){
     char CPF[14] , firstNums[3] , secondNums[3], thirdNums[3], result;
+    int expectedResult;
     printf("Put your CPF (brazzilian code to know who is the person) and I will verify if it's a valid CPF");
     scanf("%s" , CPF);
     firstNums[0] = CPF[0];
@@ -17,5 +29,11 @@ int main(){
     thirdNums[1] = CPF[9];
     thirdNums[2] = CPF[10];
     result = CPF[12];
+    expectedResult = sumBiggers(firstNums , secondNums , thirdNums)%10;
+    if (result - '0' == expectedResult){
+        printf("The digit %c is valid for this CPF" , result);
+    }else{
+        printf("The digit %c is invalid for this CPF, the valid digit is %d" , result, expectedResult);
+    }
     return 0;
 }
